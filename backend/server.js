bb690dc154d8c4546335ab21aebe0fff577a394c
yeyulchoi <yeyulchoi@outlook.com>
2024-08-08 04:10:11 -0400

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path =require('path')
 const express = require('express')
 const app = express();
 const colors= require('colors')
@@ -14,16 +15,22 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 
-app.get('/',(req,res)=>{
-    res.status(200).json({name:'man', gender:'female'})
-   
-    
-})
+
 
 //Route
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
+// Serve Frontend
+if(process.env.NODE_ENV ==='production') {
+    //set build folder as static
+    app.use(express.static(path.join(__dirname,'../frontend/build')))
+    app.get('*', (req,res)=>res.sendFile(__dirname,'../', 'frontend','build','index.html'))    // hit /api/users/ api/tickets first , and then anything else (*)
+}else{
+    app.get('/',(req,res)=>{
+        res.status(200).json({message:'Welcome to the Support Desk API'})
+    })
+}
 
 
 //error handler
